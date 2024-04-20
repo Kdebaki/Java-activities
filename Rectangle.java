e9e329aa53f81f6a009288a8924bd6aa51e6fa7a
Kdebaki <161140487+Kdebaki@users.noreply.github.com>
2024-04-20 20:53:45 +0530

    Add files via upload

---
+++ Rectangle.java
@@ -0,0 +1,56 @@
+/** ACTIVITY- 02    Implement various real time entities as classes and objects.
+Define a class Rectangle with attributes length and width. Create methods to calculate area and perimeter of the
+rectangle. Declare and initialize an object of the Rectangle class and access its attributes and methods.
+*/
+package week2;
+public class Rectangle {
+  double length;
+  double width;
+
+    // Constructor
+    public Rectangle(double length, double width) {
+        this.length = length;
+        this.width = width;
+    }
+
+    // Method to calculate area
+    public double calculateArea() {
+        return length * width;
+    }
+
+    // Method to calculate perimeter
+    public double calculatePerimeter() {
+        return 2 * (length + width);
+    }
+
+    // Getter methods
+    public double getLength() {
+        return length;
+    }
+
+    public double getWidth() {
+        return width;
+    }
+
+    // Setter methods
+    public void setLength(double length) {
+        this.length = length;
+    }
+
+    public void setWidth(double width) {
+        this.width = width;
+    }
+
+    public static void main(String[] args) {
+        // Declare and initialize an object of the Rectangle class
+        Rectangle myRectangle = new Rectangle(5, 3);
+
+        // Accessing attributes
+        System.out.println("Length: " + myRectangle.getLength());
+        System.out.println("Width: " + myRectangle.getWidth());
+
+        // Accessing methods
+        System.out.println("Area: " + myRectangle.calculateArea());
+        System.out.println("Perimeter: " + myRectangle.calculatePerimeter());
+    }
+}
